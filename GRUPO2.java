866d870001227dee0066266ed4efb13a78d3c0dd
lizcaminha <lizcaminha@gmail.com>
2025-07-07 11:25:10 -0400

    rascunho da estrutura

---
+++ GRUPO2.java
@@ -0,0 +1,40 @@
+
+
+// ainda não testei pra ver se compila, vamos terminar todas as funcionalidades primeiro.
+import java.util.Scanner;
+public class GRUPO2{
+    public static void main(){
+        Scanner leia = new Scanner(System.in);
+        //aqui vai ficar a interação com o usuário, e todos os metodos serão chamados aqui.
+        /*TODO:
+            declarar variáveis
+            interacao com usuário
+            criar as instancias de objeto
+            etc...
+        */
+       switch (int forma){ //escolha de formas!
+        case 1:
+            System.out.println("Forma escolhida: Prisma");
+            //fazer subescolha de prismas...?
+            Prisma poliedro = new Prisma(); // apenas criar o poliedro já com a base. essa linha está errada.
+            System.out.println("Qual é a altura do prisma?")
+            leia.nextInt(poliedro.altura);
+       }
+    }
+}
+public class Prisma{
+    int altura;
+    public static double volumePrisma(altura, areaBase){
+        double volume;
+        volume=(double)altura*areaBase;
+        return volume;
+    }
+}
+class prismaBaseQuadrada extends Prisma{
+    int lado1, lado2;
+    public int areaBase(){
+        int area;
+        area=lado1*lado2;
+        return area;
+    }
+}
