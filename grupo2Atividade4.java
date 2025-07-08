681726abe9073c27f9db9ed74bc5cf2d1aa5bafd
Liz S. Caminha <lizcaminha@gmail.com>
2025-07-08 17:28:16 -0400

    correção de bugs

--- grupo2Atividade4.java
+++ grupo2Atividade4.java
@@ -1,8 +1,6 @@
-
-
 // ainda não testei pra ver se compila, vamos terminar todas as funcionalidades primeiro.
 import java.util.Scanner;
-public class grupo2Atividade4{
+public class grupo2atividade4{
     public static void main(){
         Scanner leia = new Scanner(System.in);
         //aqui vai ficar a interação com o usuário, e todos os metodos serão chamados aqui.
@@ -12,19 +10,21 @@ public class grupo2Atividade4{
             criar as instancias de objeto
             etc...
         */
-       switch (int forma){ //escolha de formas!
+        int opcao;
+       switch (opcao){ //escolha de formas!
         case 1:
             System.out.println("Forma escolhida: Prisma");
             //fazer subescolha de prismas...?
             Prisma poliedro = new Prisma(); // apenas criar o poliedro já com a base. essa linha está errada.
-            System.out.println("Qual é a altura do prisma?")
+            System.out.println("Qual é a altura do prisma?");
             leia.nextInt(poliedro.altura);
+        break;
        }
     }
 }
 public class Prisma{
     int altura;
-    public static double volumePrisma(altura, areaBase){
+    public static double volumePrisma(int altura, int areaBase){
         double volume;
         volume=(double)altura*areaBase;
         return volume;
