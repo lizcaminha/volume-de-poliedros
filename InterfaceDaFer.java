4e40c32ac8a27b4fd67605a4baded969d8af91de
Fernanda Almeida Fernandes <fernanda.almeida@estudante.ifmt.edu.br>
2025-09-28 19:20:03 -0400

    Add files via upload

---
+++ InterfaceDaFer.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package interfacedafer;
+
+/**
+ *
+ * @author Aluno
+ */
+public class InterfaceDaFer {
+
+    /**
+     * @param args the command line arguments
+     */
+}
